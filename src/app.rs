ddd8ae324d5e65fdd1d5ef2646f14ab8d18ecaf9
ros <ros@noetic.myguest.virtualbox.org>
2024-10-11 17:11:35 +0500

    fix server addr

--- src/app.rs
+++ src/app.rs
@@ -18,7 +18,7 @@ pub async fn serve() {
         .route("/contacts", get(handle_contacts))
         .route("/servo", post(handle_servo));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("localhost:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
